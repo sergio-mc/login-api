b46e613b558dc4095998b6140733bdfe019cf50e
Sergio Marrón Calle <sergiodev0106@gmail.com>
2019-12-11 20:23:06 +0100

    Second commit

--- blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique()->primary();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('activo');
             $table->timestamps();
         });
     }
