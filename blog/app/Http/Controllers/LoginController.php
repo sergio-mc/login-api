06e385dc693addd53f1e8d01676675dd64f1c9a7
Sergio Marrón Calle <sergiodev0106@gmail.com>
2019-12-13 20:09:20 +0100

    Some better improvements

--- blog/app/Http/Controllers/LoginController.php
+++ blog/app/Http/Controllers/LoginController.php
@@ -10,13 +10,19 @@ class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        $decoded = json_decode($request->get('decoded'));
+        $infologin = $request->logindata;
+
+        Log::info('El usuario '.$infologin->email.' se ha registrado');
+        return response('Acceso correcto');
+
+        // 1ra forma mia de hacerlo 
+        /*$decoded = json_decode($request->get('decoded'));
         $email = $decoded->email;
         $password = $decoded->password;
 
         $logEmail = Log::notice('Email: '.$email);
         $logPassword = Log::notice('Password: '.$password);
 
-        return $request->get('decoded');
+        return $request->get('decoded');*/
     }
 }
--- blog/app/Http/Middleware/AuthLogin.php
+++ blog/app/Http/Middleware/AuthLogin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 
 use \Firebase\JWT\JWT;
+use Illuminate\Support\Facades\Log;
 use Closure;
 
 class AuthLogin
@@ -19,16 +20,25 @@ class AuthLogin
     {
 
         $key = env('JWT_KEY');
-        $decoded = JWT::decode($request->getContent(), $key, array('HS256'));
+        try{
+            $decoded = JWT::decode($request->getContent(), $key, array('HS256'));
+            $request->logindata = $decoded;
+            return $next($request);
+        }catch(\Exception $e){
+            Log::alert('Intento de acceso fallido');
+            return response('El token no es correcto',401);
+        }
+        
         $email = $decoded->email;
         $password = $decoded->password;
         $response = 'Error';
         
-        if(!empty($email) && !empty($password))
+        // 1ra forma mia de hacerlo.
+        /*if(!empty($email) && !empty($password))
         {
             $request->attributes->add(['decoded' => json_encode($decoded)]);
             return $next($request);
-        }else{return $response;}
+        }else{return $response;}*/
         
         
     }
